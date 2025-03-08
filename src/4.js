45c4b1d7d9788b445620ae407d2c28f5c6b2bed8
kyladopico06 <kyladopico06@gmx.com>
2025-03-08 05:27:41 +0000

    Add 4.js

---
+++ src/4.js
@@ -0,0 +1,7 @@
+// This is an example of a JavaScript function that takes two arguments and returns their sum
+function sum(a, b) {
+  return a + b;
+}
+
+// You can call this function by passing in values for the two arguments
+sum(3, 4); // returns 7
